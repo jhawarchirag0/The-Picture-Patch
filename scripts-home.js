ec7ee9fcfe266ce8c94984af7cd88de60360cb4e
chirag jhawar <chirag.jhavar@somaiya.edu>
2020-05-02 18:02:53 +0530

    commit

---
+++ scripts-home.js
@@ -0,0 +1,15 @@
+const slides = document.querySelector('.slideshow');
+const images = document.querySelectorAll('.slideshow img');
+
+const prev = document.querySelector("#prevBtn");
+const next = document.querySelector("#nextBtn");
+
+let counter = 1;
+const size = images[0].clientWidth;
+
+// slides.styles.transform = "translateX("+ (-size*counter)+"px)"; 
+
+
+function prevImage(){
+    
+}
