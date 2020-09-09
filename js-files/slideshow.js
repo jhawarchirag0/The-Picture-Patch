c4138fe020b40bee0c45dcf03e1d213e48d0007a
chirag jhawar <chirag.jhavar@somaiya.edu>
2020-09-09 21:05:01 +0530

    commit on signupV

---
+++ js-files/slideshow.js
@@ -0,0 +1,14 @@
+var slideIndex = 0;
+
+window.onload= function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+    console.log(10);
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 2000); 
+}
